af8428a19fea7b014de7ef699af1a71cb1c3ef20
tommy <tommy@test.com>
2022-12-13 13:32:44 -0600

    change001

---
+++ django_ve/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Tommywang/Advanced python/Djangp_Project/django_ve/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
